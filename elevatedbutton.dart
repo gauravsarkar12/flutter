cd86ec4cc2527095a2c2266d2e418ea228fc6bbc
Gaurav Sarkar <35233675+gauravsarkar12@users.noreply.github.com>
2021-08-23 23:42:05 +0530

    Update elevatedbutton.dart

--- elevatedbutton.dart
+++ elevatedbutton.dart
@@ -10,7 +10,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Ganesh Optical Co'),
+        title: Text('App'),
         centerTitle: true,
         backgroundColor: Colors.teal,
       ),
